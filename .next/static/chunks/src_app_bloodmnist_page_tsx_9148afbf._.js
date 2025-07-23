47a2df2b7bec78f92a8fedce28cd451668f3c9c2
Avelino Lupo <103952759+lino62U@users.noreply.github.com>
2025-07-23 01:09:26 -0500

    Add .next/static/chunks/src_app_bloodmnist_page_tsx_9148afbf._.js 1753250965670

---
+++ .next/static/chunks/src_app_bloodmnist_page_tsx_9148afbf._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_de95a9db._.js"
+],
+    source: "dynamic"
+});
